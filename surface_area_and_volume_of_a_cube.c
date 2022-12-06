af8c9b2f96aa4b0d0edb9ca144defa1858a46d45
Deepthigadugoyyala <119839747+Deepthigadugoyyala@users.noreply.github.com>
2022-12-06 21:32:25 +0530

    #include<stdio.h> int sv(float x); int main() {         float x;         scanf("%f",&x);         int A=sv(x); } int sv(float x) {         float a,v;         a=6*x*x;         v=x*x*x;         printf("Surface area = %0.2f and Volume = %0.2f",a,v);         return 0; }

--- surface_area_and_volume_of_a_cube.c
+++ surface_area_and_volume_of_a_cube.c
@@ -1,10 +1,16 @@
 #include<stdio.h>
+int sv(float x);
 int main()
 {
         float x;
         scanf("%f",&x);
+        int A=sv(x);
+}
+int sv(float x)
+{
         float a,v;
         a=6*x*x;
         v=x*x*x;
         printf("Surface area = %0.2f and Volume = %0.2f",a,v);
+        return 0;
 }
