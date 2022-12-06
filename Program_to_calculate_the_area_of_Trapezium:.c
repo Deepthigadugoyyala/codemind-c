a2dfdaaf127ef334349a2f038803538944793582
Deepthigadugoyyala <119839747+Deepthigadugoyyala@users.noreply.github.com>
2022-12-06 21:21:41 +0530

    #include<stdio.h> int trape(int b1,int b2,int h); int main() {     int b1,b2,h;     scanf("%d%d%d",&b1,&b2,&h);     int x=trape(b1,b2,h); } int trape(int b1,int b2,int h) {     float ar;     ar=((b1+b2)/2.0)*h;     printf("%0.4f",ar);     return 0; }

--- Program_to_calculate_the_area_of_Trapezium:.c
+++ Program_to_calculate_the_area_of_Trapezium:.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
+int trape(int b1,int b2,int h);
 int main()
 {
     int b1,b2,h;
     scanf("%d%d%d",&b1,&b2,&h);
+    int x=trape(b1,b2,h);
+}
+int trape(int b1,int b2,int h)
+{
     float ar;
     ar=((b1+b2)/2.0)*h;
     printf("%0.4f",ar);
+    return 0;
 }
