fdadaeaee7da9d5ed3b06fed1c5b707a7a4bfe8f
Deepthigadugoyyala <119839747+Deepthigadugoyyala@users.noreply.github.com>
2022-12-06 21:01:29 +0530

    #include<stdio.h> int yr(int year,int first,int second); int main() {     int year,first,second;     scanf("%d",&year);     int x=yr(year,first,second); } int yr(int year,int first,int second) {     second=year%10;     first=year%100/10;     printf("%d%d",first,second);     return 0; }

--- _Last_two_Digits_of_a_given_Year.c
+++ _Last_two_Digits_of_a_given_Year.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
+int yr(int year,int first,int second);
 int main()
 {
     int year,first,second;
     scanf("%d",&year);
+    int x=yr(year,first,second);
+}
+int yr(int year,int first,int second)
+{
     second=year%10;
     first=year%100/10;
     printf("%d%d",first,second);
