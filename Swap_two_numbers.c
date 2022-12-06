1c8a18ffdc54d86775e4222887e831af30dd872c
Deepthigadugoyyala <119839747+Deepthigadugoyyala@users.noreply.github.com>
2022-12-06 21:11:12 +0530

    #include<stdio.h> void swap(int a,int b); int main() {     int a,b;     scanf("%d%d",&a,&b);     swap(a,b); } void swap(int a,int b) {     a=a+b;     b=a-b;     a=a-b;     printf("%d %d",a,b); }

--- Swap_two_numbers.c
+++ Swap_two_numbers.c
@@ -1,17 +1,16 @@
 #include<stdio.h>
-int swap(int a,int b);
+void swap(int a,int b);
 int main()
 {
     int a,b;
     scanf("%d%d",&a,&b);
-    int x=swap(a,b);
+    swap(a,b);
 }
-int swap(int a,int b)
+void swap(int a,int b)
 {
     a=a+b;
     b=a-b;
     a=a-b;
     printf("%d
 %d",a,b);
-    return 0;
 }
