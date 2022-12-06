9d14b848b39e5f3cbe6c3b75a83bdb1a9b7081b4
Deepthigadugoyyala <119839747+Deepthigadugoyyala@users.noreply.github.com>
2022-12-06 21:43:12 +0530

    #include<stdio.h> void gs(float basic,float hra,float da,float pf,float grosssalary); int main() {     float basic,hra,da,pf,grosssalary;     scanf("%f%f%f",&basic,&hra,&da);     gs(basic,hra,da,pf,grosssalary); } void gs(float basic,float hra,float da,float pf,float grosssalary) {     pf=basic*12/100;     grosssalary=basic+hra+da+pf;     printf("%0.2f %0.2f",pf,grosssalary);
    }

--- Calculate_Gross_Salary_of_an_employee.c
+++ Calculate_Gross_Salary_of_an_employee.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
-int gs(float basic,float hra,float da,float pf,float grosssalary);
+void gs(float basic,float hra,float da,float pf,float grosssalary);
 int main()
 {
     float basic,hra,da,pf,grosssalary;
     scanf("%f%f%f",&basic,&hra,&da);
-    int x=gs(basic,hra,da,pf,grosssalary);
+    gs(basic,hra,da,pf,grosssalary);
 }
-int gs(float basic,float hra,float da,float pf,float grosssalary)
+void gs(float basic,float hra,float da,float pf,float grosssalary)
 {
     pf=basic*12/100;
     grosssalary=basic+hra+da+pf;
     printf("%0.2f
 %0.2f",pf,grosssalary);
-    return 0;
+
 }
