0626293888ab45e2f50631f96ceb69ea81c99010
Deepthigadugoyyala <119839747+Deepthigadugoyyala@users.noreply.github.com>
2022-12-06 21:38:44 +0530

    #include<stdio.h> int main() {     float basic,hra,da,pf,grosssalary;     scanf("%f%f%f",&basic,&hra,&da);     pf=basic*12/100;     grosssalary=basic+hra+da+pf;     printf("%0.2f %0.2f",pf,grosssalary); }

---
+++ Calculate_Gross_Salary_of_an_employee.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float basic,hra,da,pf,grosssalary;
+    scanf("%f%f%f",&basic,&hra,&da);
+    pf=basic*12/100;
+    grosssalary=basic+hra+da+pf;
+    printf("%0.2f
+%0.2f",pf,grosssalary);
+}
