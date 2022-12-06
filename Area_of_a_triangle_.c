076abd9c64be628f2ddcbf033908122000c7e7ca
Deepthigadugoyyala <119839747+Deepthigadugoyyala@users.noreply.github.com>
2022-12-06 21:06:01 +0530

    #include<stdio.h> void tri(int b,int h,int area); int main() {     int b,h,area;     scanf("%d%d",&b,&h);     tri(b,h,area); } void tri(int b,int h,int area) {     area=0.5*b*h;     printf("%d",area); }

--- Area_of_a_triangle_.c
+++ Area_of_a_triangle_.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
-int tri(int b,int h,int area);
+void tri(int b,int h,int area);
 int main()
 {
     int b,h,area;
     scanf("%d%d",&b,&h);
-    int x=tri(b,h,area);
+    tri(b,h,area);
 }
-int tri(int b,int h,int area)
+void tri(int b,int h,int area)
 {
     area=0.5*b*h;
     printf("%d",area);
-    return 0;
 }
