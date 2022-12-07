89090ba40bd23e34c1e86665ed22281ff5fcc142
Deepthigadugoyyala <119839747+Deepthigadugoyyala@users.noreply.github.com>
2022-12-07 10:33:30 +0530

    #include<stdio.h> void rj(int X,int Y,int Z,int n); int main() {     int X,Y,Z,n;     scanf("%d%d%d",&X,&Y,&Z);     rj(X,Y,Z,n); } void rj(int X,int Y,int Z,int n) {     n=(5*X+10*Y)/Z;     printf("%d",n); }

--- Romeo_and_Juliet.c
+++ Romeo_and_Juliet.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
-int rj(int X,int Y,int Z,int n);
+void rj(int X,int Y,int Z,int n);
 int main()
 {
     int X,Y,Z,n;
     scanf("%d%d%d",&X,&Y,&Z);
-    int x=rj(X,Y,Z,n);
+    rj(X,Y,Z,n);
 }
-int rj(int X,int Y,int Z,int n)
+void rj(int X,int Y,int Z,int n)
 {
     n=(5*X+10*Y)/Z;
     printf("%d",n);
-    return 0;
 }
