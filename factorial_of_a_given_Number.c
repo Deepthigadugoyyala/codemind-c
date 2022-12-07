1a78eb533a51c8e2e1dce95745b31ff0546f2cc0
Deepthigadugoyyala <119839747+Deepthigadugoyyala@users.noreply.github.com>
2022-12-07 12:17:41 +0530

    #include<stdio.h> int f(int fact,int n,int i); int main() {     int fact=1,n,i;     scanf("%d",&n);     int x=f(fact,n,i); } int f(int fact,int n,int i) {     for(i=1;i<=n;i++)     fact=fact*i;     {         printf("%d ",fact);     }     return 0; }

--- factorial_of_a_given_Number.c
+++ factorial_of_a_given_Number.c
@@ -1,11 +1,17 @@
 #include<stdio.h>
+int f(int fact,int n,int i);
 int main()
 {
     int fact=1,n,i;
     scanf("%d",&n);
+    int x=f(fact,n,i);
+}
+int f(int fact,int n,int i)
+{
     for(i=1;i<=n;i++)
     fact=fact*i;
     {
         printf("%d ",fact);
     }
+    return 0;
 }
