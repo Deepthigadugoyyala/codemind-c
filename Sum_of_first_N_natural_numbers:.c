7690fcbc4e710bf7da0b348731e3f5534b4e02bb
Deepthigadugoyyala <119839747+Deepthigadugoyyala@users.noreply.github.com>
2022-12-07 10:44:37 +0530

    #include<stdio.h> int N(int i,int n,int sum); int main() {     int i,n,sum=0;     scanf("%d",&n);     int x=N(i,n,sum); } int N(int i,int n,int sum) {     for(i=1;i<=n;i++)     {         sum=sum+i;     }     {         printf("%d",sum);     }     return 0; }

--- Sum_of_first_N_natural_numbers:.c
+++ Sum_of_first_N_natural_numbers:.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
+int N(int i,int n,int sum);
 int main()
 {
     int i,n,sum=0;
     scanf("%d",&n);
+    int x=N(i,n,sum);
+}
+int N(int i,int n,int sum)
+{
     for(i=1;i<=n;i++)
     {
         sum=sum+i;
@@ -10,4 +15,5 @@ int main()
     {
         printf("%d",sum);
     }
+    return 0;
 }
