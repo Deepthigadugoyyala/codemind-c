6c7a787e38b9b04a26c31d22e3b71aa13e5a3c06
Deepthigadugoyyala <119839747+Deepthigadugoyyala@users.noreply.github.com>
2022-12-07 10:39:02 +0530

    #include<stdio.h> int hm(int hrs,int mins,int n); int main() {     int hrs,mins,n;     scanf("%d",&n);     int x=hm(hrs,mins,n); } int hm(int hrs,int mins,int n) {     hrs=n/60;     mins=n%60;     printf("%d Hour(s) %d Minute(s)",hrs,mins);     return 0; }

--- HOURS_and_MINUTES.c
+++ HOURS_and_MINUTES.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
+int hm(int hrs,int mins,int n);
 int main()
 {
     int hrs,mins,n;
     scanf("%d",&n);
+    int x=hm(hrs,mins,n);
+}
+int hm(int hrs,int mins,int n)
+{
     hrs=n/60;
     mins=n%60;
     printf("%d Hour(s) %d Minute(s)",hrs,mins);
+    return 0;
 }
