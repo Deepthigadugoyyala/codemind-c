af41ac19e0340d95f2195c864f7181fdcf4f97e5
Deepthigadugoyyala <119839747+Deepthigadugoyyala@users.noreply.github.com>
2022-12-12 12:00:06 +0530

    #include<stdio.h> int ar(int n,int odd); int main() {     int n,odd=0;     scanf("%d",&n);     int a=ar(n,odd); } int ar(int n,int odd) {     int x[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&x[i]);     }     for(i=0;i<n;i++)     {         if(x[i]%2!=0)         odd=odd+x[i];     }     printf("%d",odd);     return 0; }

---
+++ ODD_Sum.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int ar(int n,int odd);
+int main()
+{
+    int n,odd=0;
+    scanf("%d",&n);
+    int a=ar(n,odd);
+}
+int ar(int n,int odd)
+{
+    int x[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&x[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(x[i]%2!=0)
+        odd=odd+x[i];
+    }
+    printf("%d",odd);
+    return 0;
+}
