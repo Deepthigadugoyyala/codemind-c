2b676bc9d4c90fc7304007df1b5959042ab3304b
Deepthigadugoyyala <119839747+Deepthigadugoyyala@users.noreply.github.com>
2022-12-12 10:31:42 +0530

    #include<stdio.h> int palin(int n,int r,int rev,int s); int main() {     int n,r,rev=0,s;     scanf("%d",&n);     int x=palin(n,r,rev,s); } int palin(int n,int r,int rev,int s) {      s=n;     while(n!=0)     {         r=n%10;         rev=rev*10+r;         n=n/10;     }     if(s==rev)     printf("Palindrome");     else     printf("Not Palindrome");     return 0; }

--- Palindrome.c
+++ Palindrome.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
+int palin(int n,int r,int rev,int s);
 int main()
 {
-    int n,r,rev=0;
+    int n,r,rev=0,s;
     scanf("%d",&n);
-    int s=n;
+    int x=palin(n,r,rev,s);
+}
+int palin(int n,int r,int rev,int s)
+{
+     s=n;
     while(n!=0)
     {
         r=n%10;
@@ -14,4 +19,5 @@ int main()
     printf("Palindrome");
     else
     printf("Not Palindrome");
+    return 0;
 }
