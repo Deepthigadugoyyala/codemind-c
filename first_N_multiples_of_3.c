fdf368476a074f31a4002ab59aa4675d9e74b7b8
Deepthigadugoyyala <119839747+Deepthigadugoyyala@users.noreply.github.com>
2022-12-12 09:59:58 +0530

    #include<stdio.h> void N(int i,int n); int main() {     int i,n;     scanf("%d",&n);     N(i,n); } void N(int i,int n) {     for(i=1;i<=n;i++)     {             printf("%d ",i*3);     }
    }

--- first_N_multiples_of_3.c
+++ first_N_multiples_of_3.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
-int N(int i,int n);
+void N(int i,int n);
 int main()
 {
     int i,n;
     scanf("%d",&n);
-    int x=N(i,n);
+    N(i,n);
 }
-int N(int i,int n)
+void N(int i,int n)
 {
     for(i=1;i<=n;i++)
     {
             printf("%d ",i*3);
     }
-    return 0;
+    
 }
