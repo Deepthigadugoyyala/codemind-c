308e0987c0b8605dbdbe96359544749467184d02
Deepthigadugoyyala <119839747+Deepthigadugoyyala@users.noreply.github.com>
2022-12-12 09:56:05 +0530

    #include<stdio.h> int nat(int i,int n); int main() {     int i,n;     scanf("%d",&n);     int x=nat(i,n); } int nat(int i,int n) {     for(i=n*2;i>=1;i--)     {         if(i%2==0)         {             printf("%d ",i);         }     }     return 0; }

--- First_N_even_Natural_Numbers.c
+++ First_N_even_Natural_Numbers.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
+int nat(int i,int n);
 int main()
 {
     int i,n;
     scanf("%d",&n);
+    int x=nat(i,n);
+}
+int nat(int i,int n)
+{
     for(i=n*2;i>=1;i--)
     {
         if(i%2==0)
@@ -10,4 +15,5 @@ int main()
             printf("%d ",i);
         }
     }
+    return 0;
 }
