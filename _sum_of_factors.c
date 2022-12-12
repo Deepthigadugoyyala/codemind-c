4e03449940f4ec2b37abc79daefd6cfd843e6c73
Deepthigadugoyyala <119839747+Deepthigadugoyyala@users.noreply.github.com>
2022-12-12 10:41:46 +0530

    #include<stdio.h> int fac(int n,int i,int s); int main() {     int n,i,s=0;     scanf("%d",&n);     int x=fac(n,i,s); } int fac(int n,int i,int s) {     for(i=1;i<n;i++)     {         if(n%i==0)         {             s=s+i;         }     }     printf("%d ",s);     return 0; }

--- _sum_of_factors.c
+++ _sum_of_factors.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
+int fac(int n,int i,int s);
 int main()
 {
     int n,i,s=0;
     scanf("%d",&n);
+    int x=fac(n,i,s);
+}
+int fac(int n,int i,int s)
+{
     for(i=1;i<n;i++)
     {
         if(n%i==0)
@@ -11,4 +16,5 @@ int main()
         }
     }
     printf("%d ",s);
+    return 0;
 }
