710f5cdf73928adbcc680985c0cb30248d7984ed
Deepthigadugoyyala <119839747+Deepthigadugoyyala@users.noreply.github.com>
2022-12-12 10:25:10 +0530

    #include<stdio.h> int c(int M,int N,int i,int count); int main() {     int M,N,i,count=0;     scanf("%d%d",&M,&N);     int x=c(M,N,i,count); } int c(int M,int N,int i,int count) {     for(i=M;i<=N;i++)     {         if(i%2 == 0 && i%3 == 0)         {             count++;         }     }     printf("%d",count);     return 0; }

--- count_of_numbers_between_M_and_N_which_divisible_by_both_2_and_3.c
+++ count_of_numbers_between_M_and_N_which_divisible_by_both_2_and_3.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
+int c(int M,int N,int i,int count);
 int main()
 {
     int M,N,i,count=0;
     scanf("%d%d",&M,&N);
+    int x=c(M,N,i,count);
+}
+int c(int M,int N,int i,int count)
+{
     for(i=M;i<=N;i++)
     {
         if(i%2 == 0 && i%3 == 0)
@@ -11,4 +16,5 @@ int main()
         }
     }
     printf("%d",count);
+    return 0;
 }
