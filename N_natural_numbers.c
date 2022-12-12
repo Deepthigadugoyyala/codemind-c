35566d66e30cd620f2c10ea11f3587f305dc3a07
Deepthigadugoyyala <119839747+Deepthigadugoyyala@users.noreply.github.com>
2022-12-12 09:49:50 +0530

    #include<stdio.h> int nat(int i,int n); int main() {     int i,n;     scanf("%d",&n);     int x=nat(i,n); } int nat(int i,int n) {     for(i=1;i<=n;i++)     {         printf("%d ",i);     }     return 0; }

--- N_natural_numbers.c
+++ N_natural_numbers.c
@@ -1,10 +1,16 @@
 #include<stdio.h>
+int nat(int i,int n);
 int main()
 {
     int i,n;
     scanf("%d",&n);
+    int x=nat(i,n);
+}
+int nat(int i,int n)
+{
     for(i=1;i<=n;i++)
     {
         printf("%d ",i);
     }
+    return 0;
 }
