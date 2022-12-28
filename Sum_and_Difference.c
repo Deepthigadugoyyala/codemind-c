841e15ede5ef64d1526ebf8b4388735d3a44aa81
Deepthigadugoyyala <119839747+Deepthigadugoyyala@users.noreply.github.com>
2022-12-28 18:41:26 +0530

    #include<stdio.h> void sd(int a1,int a2); int main() {      int a1,a2;      scanf("%d%d",&a1,&a2);      sd(a1,a2); } void sd(int a1,int a2) {      float a3,a4;      scanf("%f%f",&a3,&a4);      printf("%d %d ",a1+a2,a1-a2);      printf("%0.1f %0.1f",a3+a4,a3-a4); }

--- Sum_and_Difference.c
+++ Sum_and_Difference.c
@@ -1,17 +1,16 @@
 #include<stdio.h>
-int sd(int a1,int a2);
+void sd(int a1,int a2);
 int main()
 {
      int a1,a2;
      scanf("%d%d",&a1,&a2);
-     int x=sd(a1,a2);
+     sd(a1,a2);
 }
-int sd(int a1,int a2)
+void sd(int a1,int a2)
 {
      float a3,a4;
      scanf("%f%f",&a3,&a4);
      printf("%d %d
 ",a1+a2,a1-a2);
      printf("%0.1f %0.1f",a3+a4,a3-a4);
-     return 0;
 }
