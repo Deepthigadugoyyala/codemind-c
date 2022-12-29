fcec79b82998eef3e06523f083918ac01da2ef01
Deepthigadugoyyala <119839747+Deepthigadugoyyala@users.noreply.github.com>
2022-12-29 21:27:22 +0530

    #include<stdio.h> #include<math.h> int series(int n); int main() {     int n;     scanf("%d",&n);     int x=series(n); } int series(int n) {     int a,b,i,k1=0,k2=0;     for(i=1;i<=n;i++)     {         if(i%2==0)         {             a=pow(3,k1);             printf("%d ",a);             k1++;         }         else         {             b=pow(2,k2);             printf("%d ",b);             k2++;         }     }     return 0; }

--- Mixture_of_two_series.c
+++ Mixture_of_two_series.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
 #include<math.h>
+int series(int n);
 int main()
 {
-    int a,b,i,n,k1=0,k2=0;
+    int n;
     scanf("%d",&n);
+    int x=series(n);
+}
+int series(int n)
+{
+    int a,b,i,k1=0,k2=0;
     for(i=1;i<=n;i++)
     {
         if(i%2==0)
@@ -19,4 +25,5 @@ int main()
             k2++;
         }
     }
+    return 0;
 }
